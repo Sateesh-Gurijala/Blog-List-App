a084d3a9cb6aaedbd17912417b9a0bef351a8eca
Sateesh-Gurijala <satishgurijala007@gmail.com>
2023-12-13 05:43:22 +0000

    bloglist

---
+++ src/components/UserInfo/index.js
@@ -0,0 +1,16 @@
+// Write your JS code here
+import './index.css'
+
+const UserInfo = () => (
+  <div className="user-info-container">
+    <img
+      alt="profile icon"
+      className="profile-image"
+      src="https://assets.ccbp.in/frontend/react-js/profile-img.png"
+    />
+    <h1 className="profile-image">Wade Warren</h1>
+    <p className="profile-image">Software developer at UK</p>
+  </div>
+)
+
+export default UserInfo
